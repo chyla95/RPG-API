76a0f3b71dc936196a58c76d05238242aa907149
chyla95 <chyla95@gmail.com>
2022-12-03 02:54:28 +0100

    remove roles requirement

--- RPG.Domain/Model/General/StaffMember.cs
+++ RPG.Domain/Model/General/StaffMember.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace RPG.Domain.Model.General
+﻿namespace RPG.Domain.Model.General
 {
 #pragma warning disable CS8618
     public class StaffMember : User
     {
-        [Required]
         public IEnumerable<Role> Roles { get; set; }
     }
 #pragma warning restore CS8618
