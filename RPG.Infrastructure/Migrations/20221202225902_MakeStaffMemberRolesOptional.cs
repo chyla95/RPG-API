d70b8a32f53d882028ee6d5ba906450b0189e77b
chyla95 <chyla95@gmail.com>
2022-12-03 02:53:59 +0100

    remove StaffMember role requirement

---
+++ RPG.Infrastructure/Migrations/20221202225902_MakeStaffMemberRolesOptional.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RPG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeStaffMemberRolesOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
