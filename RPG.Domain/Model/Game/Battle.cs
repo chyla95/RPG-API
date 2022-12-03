a4d195cf8bf314957212de7fb85218fab6ed434d
chyla95 <chyla95@gmail.com>
2022-12-03 02:54:56 +0100

    add battle model

---
+++ RPG.Domain/Model/Game/Battle.cs
@@ -0,0 +1,54 @@
+﻿namespace RPG.Domain.Model.Game
+{
+    public class Battle
+    {
+        public Character Attacker { get; private set; }
+        public Character Defender { get; private set; }
+        public Character? Winner { get; private set; }
+        public Character? Loser { get; private set; }
+        public bool IsTie { get; private set; } = false;
+        public List<BattleTurn> BattleTurns { get; private set; } = new();
+
+        public Battle(Character attacker, Character defender)
+        {
+            Attacker = attacker;
+            Defender = defender;
+            SimulateFight();
+        }
+
+        private void SimulateFight()
+        {
+            bool isFightInProgress = true;
+            int attackerHitPoints = Attacker.HitPoints;
+            int defenderHitPoints = Defender.HitPoints;
+
+            while (isFightInProgress)
+            {
+                BattleTurn battleTurn = new BattleTurn(Attacker, Defender);
+                BattleTurns.Add(battleTurn);
+
+                defenderHitPoints -= battleTurn.AttackerDamage;
+                attackerHitPoints -= battleTurn.DefenderDamage;
+
+                if (defenderHitPoints <= 0 || attackerHitPoints <= 0)
+                {
+                    isFightInProgress = false;
+                    if (defenderHitPoints <= 0 && attackerHitPoints > 0)
+                    {
+                        Winner = Attacker;
+                        Loser = Defender;
+                    }
+                    if (attackerHitPoints <= 0 && defenderHitPoints > 0)
+                    {
+                        Winner = Defender;
+                        Loser = Attacker;
+                    }
+                    if (attackerHitPoints <= 0 && defenderHitPoints <= 0)
+                    {
+                        IsTie = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RPG.Domain/Model/Game/BattleTurn.cs
@@ -0,0 +1,23 @@
+﻿namespace RPG.Domain.Model.Game
+{
+    public class BattleTurn
+    {
+        private Character Attacker { get; set; }
+        private Character Defender { get; set; }
+        public int AttackerDamage { get; private set; }
+        public int DefenderDamage { get; private set; }
+
+        public BattleTurn(Character attacker, Character defender)
+        {
+            Attacker = attacker;
+            Defender = defender;
+            CalculateDamage();
+        }
+
+        private void CalculateDamage()
+        {
+            AttackerDamage = Attacker.CalculateWeaponDamage(Defender);
+            DefenderDamage = Defender.CalculateWeaponDamage(Attacker);
+        }
+    }
+}
